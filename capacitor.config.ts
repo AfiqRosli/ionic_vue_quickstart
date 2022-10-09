ea5a2a11c8a4d8f98ff715d8f0452b03c133e228
AfiqRosli <afiq_rosli@live.com>
2022-10-09 21:17:57 +0800

    rename project

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic_docs',
-  webDir: 'dist',
-  bundledWebRuntime: false
+  appId: "io.ionic.starter",
+  appName: "ionic_vue_quickstart",
+  webDir: "dist",
+  bundledWebRuntime: false,
 };
 
 export default config;
